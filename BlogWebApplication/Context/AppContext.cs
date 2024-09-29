48a6d575ff147f6c64ff0df91496dc4afb8d7f9e
oRodrigoBorges <rodrigo.odev@gmail.com>
2024-09-29 11:03:40 -0300

    Criação da Context, clase comentario, mapeamento da tabela e string de conexão

---
+++ BlogWebApplication/Context/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebApplication.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Comentario> Comentarios { get; set; }
+    }
+}
---
+++ BlogWebApplication/Models/Comentario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogWebApplication.Models
+{
+    [Table("Comentarios")] // Define a classe comentario como objetos da tabela comentários
+    public class Comentario
+    {
+        [Key] // Número inteiro que servirá como chave primária da tabela
+        public int ComentarioId { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo do nome são 100 caracteres")]
+        [Display(Name = "Seu nome aqui")]
+        public string NomeUsuario { get; set; }
+
+        [StringLength(500, ErrorMessage = "O tamanho máximo do comentário é de 500 caracteres")]
+        [Display(Name = "Insira seu comentário")]
+        public string TextoComentario { get; set; }
+    }
+}
