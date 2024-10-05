17a7e2da0723480ea21771eb7b5b2be27b057ad0
oRodrigoBorges <rodrigo.odev@gmail.com>
2024-10-05 15:31:32 -0300

    Criaação do controlador Destinos e páginas relacionadas em suas primeiras versões

---
+++ BlogWebApplication/Controllers/Destinos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApplication.Controllers
+{
+    public class Destinos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PraiaDoUna()
+        {
+            return View();
+        }
+        public IActionResult CamposDoJordao()
+        {
+            return View();
+        }
+    }
+}
