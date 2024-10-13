763d7771e5f90ac3a78bf413f6c3be4ca10dc740
oRodrigoBorges <rodrigo.odev@gmail.com>
2024-10-13 15:34:28 -0300

    Adição de API para retorno de condições atuais de tempo

--- BlogWebApplication/Controllers/DestinosController.cs
+++ BlogWebApplication/Controllers/DestinosController.cs
@@ -49,12 +49,17 @@
 using BlogWebApplication.Context;
 using BlogWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BlogWebApplication.Controllers
 {
     public class DestinosController : Controller
     {
         private readonly BlogAppDbContext _context;
+        private readonly string _weatherApiKey = "5e3cb400abca22d194651d6d362ebe43"; // API de Previsão do Tempo
+        private readonly string _weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather"; // API de Previsão do Tempo
 
         public DestinosController(BlogAppDbContext context)
         {
@@ -71,12 +76,17 @@ namespace BlogWebApplication.Controllers
             return View();
         }
 
-        public IActionResult CamposDoJordao()
+        public async Task<IActionResult> CamposDoJordaoAsync()
         {
             // Carregar os comentários do banco de dados
             var comentarios = _context.Comentarios.ToList();
             ViewBag.Comentarios = comentarios;
             ViewBag.HasComentarios = comentarios.Any(); // Verifica se existem comentários
+
+            // Chamar a API de previsão do tempo
+            var weatherData = await GetWeatherDataAsync("Campos do Jordão");
+            ViewBag.WeatherData = weatherData;
+
             return View();
         }
 
@@ -91,6 +101,18 @@ namespace BlogWebApplication.Controllers
             }
 
             return View("CamposDoJordao");
+        }
+
+        // Método para buscar dados da API do OpenWeatherMap
+        private async Task<dynamic> GetWeatherDataAsync(string cidade)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var url = $"{_weatherApiUrl}?q={cidade}&appid={_weatherApiKey}&units=metric&lang=pt_br";
+                var response = await httpClient.GetStringAsync(url);
+                dynamic weatherData = JsonConvert.DeserializeObject(response);
+                return weatherData;
+            }
         }
     }
 }
