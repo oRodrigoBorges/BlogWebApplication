a0b62f958b5aa0741c16fc974f34ead30f7ae52d
oRodrigoBorges <rodrigo.odev@gmail.com>
2024-11-09 20:37:31 -0300

    Implementação dos testes unitarios e de integração

---
+++ TestsBlogWebApplication.Tests/Tests/IntegrationTests/ComentarioIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using Microsoft.EntityFrameworkCore;
+using BlogWebApplication.Context;
+using BlogWebApplication.Models;
+using System.Threading.Tasks;
+
+namespace TestsBlogWebApplication.Tests.Tests.IntegrationTests
+{
+    public class ComentarioIntegrationTests
+    {
+        private DbContextOptions<BlogAppDbContext> CreateInMemoryDatabaseOptions()
+        {
+            return new DbContextOptionsBuilder<BlogAppDbContext>()
+                .UseInMemoryDatabase(databaseName: "BlogAppDbContext")
+                .Options;
+        }
+
+        [Fact]
+        public async Task AdicionarComentario_DeveSalvarNoBancoDeDados()
+        {
+            var options = CreateInMemoryDatabaseOptions();
+            using var context = new BlogAppDbContext(options);
+            var comentario = new Comentario
+            {
+                NomeUsuario = "NovoUsuarioDaClasseDeTeste",
+                TextoComentario = "Texto a ser testado na classe de teste"
+            };
+
+            context.Comentarios.Add(comentario);
+            await context.SaveChangesAsync();
+
+            var comentarioSalvo = await context.Comentarios.FirstOrDefaultAsync(c => c.NomeUsuario == "NovoUsuarioDaClasseDeTeste");
+            Assert.NotNull(comentarioSalvo);
+            Assert.Equal("Texto a ser testado na classe de teste", comentarioSalvo.TextoComentario);
+        }
+    }
+}
---
+++ TestsBlogWebApplication.Tests/Tests/UnitTests/ComentarioTests.cs
@@ -0,0 +1,23 @@
+﻿using BlogWebApplication.Models;
+using Xunit;
+
+namespace TestsBlogWebApplication.Tests.Tests.UnitTests
+{
+    public class ComentarioTests
+    {
+        [Fact]
+        public void CriarComentario_DeveVerificarSeOComentarioEstaDentroDasRegrasDefinidasNaClasse()
+        {
+            var comentario = new Comentario
+            {
+                ComentarioId = 1,
+                NomeUsuario = "Rodrigo C Borges",
+                TextoComentario = "Campos do Jordão é um lugar que desejo visitar em um futuro próximo"
+            };
+
+            Assert.Equal(1, comentario.ComentarioId);
+            Assert.Equal("Rodrigo C Borges", comentario.NomeUsuario);
+            Assert.Equal("Campos do Jordão é um lugar que desejo visitar em um futuro próximo", comentario.TextoComentario);
+        }
+    }
+}
